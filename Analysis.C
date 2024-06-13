209b5d7ba343a6c34685b802482675f3029a4c16
Computing2211 <sam@gbsm.eclipse.co.uk>
2024-06-13 17:06:44 +0100

    Comments

--- Analysis.C
+++ Analysis.C
@@ -11,7 +11,7 @@ void Analysis(){
   TString particle = "Jpsi"; //Jpsi or Psi2S
   TString prod = "NP"; //P or NP
   TString rapidity = "1"; //1,2,3
-  double lambda = -0.55; //between -1 and 1, 0 doesn't work due to /0 errors
+  double lambda = -0.55; //between -1 and 1, 0 doesn't work due to  div 0 errors
   int low = 20; //no lower than 8
   int high = 200; //no higher than 360
 
@@ -19,29 +19,34 @@ void Analysis(){
   //Graphs the raw data for the particle in question
 
   //Ratio(particle, prod, rapidity, low, high);
+  //Graphs ratio of data to fit
 
   //Pull(particle, prod, rapidity, low, high);
+  //Distance divided by error
 
   //All(particle, prod, rapidity, low, high);
+  //All three graphs in one
 
   //Corrections(lambda);
+  //Correction factors histogram
 
+
+  //VV As above but with corrected data
   //CorrectedGraph_Data(particle, prod, rapidity, lambda, low, high, true);
 
   //CorrectedRatio(particle, prod, rapidity, lambda, low, high);
 
   //CorrectedPull(particle, prod, rapidity, lambda, low, high);
 
-  CorrectedAll(particle, prod, rapidity, lambda, low, high);
-  //CorrectedAllPaper(particle, prod, rapidity, lambda, low, high);
+  CorrectedAll(particle, prod, rapidity, lambda, low, high); //Has been tweaked to produce desired graphs for report
 
-  //Chi2graph(particle, prod, rapidity, low, high);
+  //Chi2graph(particle, prod, rapidity, low, high); //Scans all lambda to find minimum. Also graphs
 
-  //CorrectedAll_Omit(particle, prod, rapidity, lambda, low, high);
+  //CorrectedAll_Omit(particle, prod, rapidity, lambda, low, high); //Omits mid bins
 
   //Chi2graph_Omit(particle, prod, rapidity, low, high);
 
   //Specific_Corrections(particle, prod, rapidity, lambda, low, high, true);
 
-  //Correlations();
+  //Correlations(); //Determines values including errors with correlation possibilities. No graphs.
 }
